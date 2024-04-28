38f83b919c28502f82d749c2c01f772d8d261e2e
ekhanal <elinakhanal467@gmail.com>
2024-04-28 08:30:16 +0545

    snippets

---
+++ src/components/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800
+    to-black text-white'>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col
+      justify-center w-full h-full'>
+        <div className='pb-4'>
+       <p className='text-4xl font-bold inline border-b-4
+       border-gray-500'>
+        About</p>
+        </div>
+        <p className='text-xl mt-10'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        A iste possimus numquam aliquid eligendi. Nostrum ullam in debitis! Nesciunt harum reiciendis ea vitae.
+        Cumque obcaecati soluta sit! Nesciunt quasi expedita inventore ad soluta nam. Reiciendis, 
+        quaerat adipisci eos error incidunt unde nostrum aspernatur ipsum rerum vitae nam dolores pariatur voluptatibus.
+        </p>
+        <br />
+        <p className='text-xl'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        A iste possimus numquam aliquid eligendi. Nostrum ullam in debitis! Nesciunt harum reiciendis ea vitae.
+        Cumque obcaecati soluta sit! Nesciunt quasi expedita inventore ad soluta nam. Reiciendis, 
+        quaerat adipisci eos error incidunt unde nostrum aspernatur ipsum rerum vitae nam dolores pariatur voluptatibus.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import photo1 from '../assets/Photo1.jpeg';
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { Link } from 'react-scroll';
+
+const Home = () => {
+  return (
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 flex justify-center items-center'>
+      <div className='max-w-screen-lg max-auto flex flex-col items-center justify-center h-full px-10 md:flex-row'>
+        <div className='flex flex-col justify-center text-center'>
+          <h2 className='text-4xl sm:text-7xl font-bold text-white'>  
+            I'm learning MERN
+          </h2>
+          <p className='text-gray-500 py-4 px-1 max-w-md text-center text-justify'>
+            Currently, I love to work on web application
+            using technologies like React, Tailwind, and JavaScript.
+          </p>
+          <div>
+            <Link 
+              to='portfolio'
+              smooth duration={500}
+              className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+              Portfolio
+              <span className='group-hover:rotate-90 duration-300'>
+                <MdKeyboardArrowRight size={25} className='ml-1'/>
+              </span>   
+            </Link>
+          </div>
+        </div>
+        <div className="mt-8 md:mt-0"> {/* Added margin top for larger screens */}
+          <img src={photo1} alt="my profile" className='rounded-2xl mx-auto w-2/3 md:w-full'/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
